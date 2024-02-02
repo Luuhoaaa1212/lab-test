690dcddb39b89ad364533b6928fd5460f9964d1c
Luuhoaaa1212 <hahahawe1239@gmail.com>
2024-02-02 22:02:14 +0700

    update url api

--- src/app/constant.ts
+++ src/app/constant.ts
@@ -1,4 +1,4 @@
 export const getUrlBaseApi = (name:string):string => {
-    return `http://localhost:3001/v1/${name}`;
+    return `https://shopvh-co-server.onrender.com/v1/${name}`;
 }
-export const URL_UPLOAD_IMG:string = 'http://localhost:3001/uploads/'
+export const URL_UPLOAD_IMG:string = 'https://shopvh-co-server.onrender.com/uploads/'
